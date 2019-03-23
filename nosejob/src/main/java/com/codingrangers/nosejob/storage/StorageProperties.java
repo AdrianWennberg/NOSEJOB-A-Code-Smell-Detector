3092e8514b8a45f0e0073d83f3d452f465c11ad4
William Akinsanya <akinsanya.will@gmail.com>
2019-03-23 16:56:40 +0000

    check for path validity

--- nosejob/src/main/java/com/codingrangers/nosejob/storage/StorageProperties.java
+++ nosejob/src/main/java/com/codingrangers/nosejob/storage/StorageProperties.java
@@ -2,14 +2,24 @@ package com.codingrangers.nosejob.storage;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 @Configuration
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
-	private String location = "nosejob/src/codestore";
+	private String location;
 
 	public String getLocation() {
+		Path unixPath = Paths.get("nosejob", "src", "codestore");
+		Path windowsPath = Paths.get("src", "codestore");
+		if (Files.exists(unixPath)) {
+			this.location = unixPath.toString();
+		} else if (Files.exists(windowsPath)) {
+			this.location = windowsPath.toString();
+		}
 		return location;
 	}
 
