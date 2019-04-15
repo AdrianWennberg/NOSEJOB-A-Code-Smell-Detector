c18cccb35ce521b200649cc94eec9646ff920ae5
AleBaccin <baccin.alessandro@gmail.com>
2019-04-15 21:34:51 +0100

    Violation of Data privacy implemented, need testing

--- nosejob/src/main/java/com/codingrangers/nosejob/sniffers/ProjectSniffer.java
+++ nosejob/src/main/java/com/codingrangers/nosejob/sniffers/ProjectSniffer.java
@@ -3,6 +3,8 @@ package com.codingrangers.nosejob.sniffers;
 public class ProjectSniffer extends SnifferBase {
 	public ProjectSniffer() {
 		PrimitiveObsessionSniffer primitiveObsessionAnalyser = new PrimitiveObsessionSniffer();
+		BloatedCodeSniffer bloatedCodeSniffer = new BloatedCodeSniffer();
 		this.analysers.add(primitiveObsessionAnalyser);
+		this.analysers.add(bloatedCodeSniffer);
 	}
 }
