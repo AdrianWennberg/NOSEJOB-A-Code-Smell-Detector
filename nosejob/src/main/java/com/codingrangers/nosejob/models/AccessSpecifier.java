3be4326b64fe3c780f7fcd62948ea180479236d8
AleBaccin <baccin.alessandro@gmail.com>
2019-04-15 21:34:51 +0100

    Violation of Data privacy implemented, need testing

---
+++ nosejob/src/main/java/com/codingrangers/nosejob/models/AccessSpecifier.java
@@ -0,0 +1,4 @@
+package com.codingrangers.nosejob.models;
+
+public interface AccessSpecifier {
+}
--- nosejob/src/test/java/com/codingrangers/nosejob/sniffers/ProjectSnifferTest.java
+++ nosejob/src/test/java/com/codingrangers/nosejob/sniffers/ProjectSnifferTest.java
@@ -12,13 +12,15 @@ public class ProjectSnifferTest {
     public static class getProjectReportTest{
         @Test
         public void getProjectReportTest(){
-            String primitiveObsessionName = "Primitive Obsession";
+            String bloatedCodeName = "Bloated Code";
             ParsedProject testProject = new ParsedProject();
 
             ProjectSniffer globalReport = new ProjectSniffer();
             globalReport.setProjectToAnalyse(testProject);
 
-            assertEquals(primitiveObsessionName, globalReport.getProjectReport().getSmellNames().get(0));
+            assertEquals(bloatedCodeName, globalReport.getProjectReport().getSmellNames().get(0));
+            assertEquals(0f, globalReport.getProjectReport().getSmellReports().get(0).getTotalSmellSeverity(), 0.01);
+            assertEquals(0f, globalReport.getProjectReport().getSmellReports().get(1).getTotalSmellSeverity(), 0.01);
         }
     }
 }
