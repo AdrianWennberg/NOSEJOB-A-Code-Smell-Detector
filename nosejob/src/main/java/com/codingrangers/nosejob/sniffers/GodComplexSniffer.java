64f6953f6e4210fa8addcf9c95889649ecceb011
AleBaccin <baccin.alessandro@gmail.com>
2019-04-16 09:28:56 +0100

    God Complex Sniffer implemented, need testing

--- nosejob/src/main/java/com/codingrangers/nosejob/sniffers/GodComplexSniffer.java
+++ nosejob/src/main/java/com/codingrangers/nosejob/sniffers/GodComplexSniffer.java
@@ -1,4 +1,19 @@
 package com.codingrangers.nosejob.sniffers;
 
-public class GodComplexSniffer {
+import com.codingrangers.nosejob.models.SmellReportBody;
+import com.codingrangers.nosejob.reports.SmellReport;
+
+public class GodComplexSniffer extends GeneralSniffer {
+    private static final String NAME = "God Complex";
+
+    @Override
+    public SmellReportBody getSmellReport() {
+        /**TODO
+         * retrieveSmellsFromClasses();*/
+
+        SmellReport report = new SmellReport();
+        report.setSmellName(NAME);
+        report.addSmells(smells);
+        return report;
+    }
 }
