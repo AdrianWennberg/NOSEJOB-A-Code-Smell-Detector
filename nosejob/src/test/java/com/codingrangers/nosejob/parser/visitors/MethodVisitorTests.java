5f254b39565dca959f51c510393a0ae9522a9965
Angrymajor <peterfmajor@gmail.com>
2019-04-19 11:42:23 +0100

    added tests for varaible and method identification

--- nosejob/src/test/java/com/codingrangers/nosejob/parser/visitors/MethodVisitorTests.java
+++ nosejob/src/test/java/com/codingrangers/nosejob/parser/visitors/MethodVisitorTests.java
@@ -1,5 +1,51 @@
 package com.codingrangers.nosejob.parser.visitors;
 
-public class MethodVisitorTests {
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.codingrangers.nosejob.parser.data.ParsedMethod;
+import com.codingrangers.nosejob.parser.data.ParsedVariable;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+public class MethodVisitorTests {
+	
+	MethodVisitor visitor;
+	CompilationUnit compUnit;
+	ParsedMethod methodData;
+	
+	@Before
+	public void before() {
+		visitor = new MethodVisitor(Mockito.mock(VariableVisitor.class));
+		methodData = Mockito.mock(ParsedMethod.class);
+	}
+	
+	void setCompUnit(String fileName) {
+		File file = new File(fileName);
+		try {
+			compUnit = JavaParser.parse(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void identificationTest() {
+		setCompUnit("src/test/ParserTestTargets/MethodIdentificationTestTarget.java");
+		
+		visitor.visit(compUnit, methodData);
+		
+		verify(methodData, times(2)).setReturnType("void", false);
+		verify(methodData).setReturnType("int", true);
+		verify(methodData).setReturnType("MethodIdentificationTestTarget", false);
+
+		
+	}
 }
--- nosejob/src/test/java/com/codingrangers/nosejob/parser/visitors/VariableVisitorTests.java
+++ nosejob/src/test/java/com/codingrangers/nosejob/parser/visitors/VariableVisitorTests.java
@@ -1,5 +1,51 @@
 package com.codingrangers.nosejob.parser.visitors;
 
-public class VariableVisitorTests {
+import java.io.File;
+import java.io.FileNotFoundException;
 
+import org.junit.*;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+
+import com.codingrangers.nosejob.parser.data.ParsedVariable;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.VariableDeclarator;
+
+
+
+public class VariableVisitorTests {
+	
+	VariableVisitor visitor;
+	CompilationUnit compUnit;
+	ParsedVariable variableData;
+	
+	@Before
+	public void before() {
+		visitor = new VariableVisitor();
+		variableData = Mockito.mock(ParsedVariable.class);
+	}
+	
+	void setCompUnit(String fileName) {
+		File file = new File(fileName);
+		try {
+			compUnit = JavaParser.parse(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void identificationTest() {
+		setCompUnit("src/test/ParserTestTargets/VariableIdentificationTestTarget.java");
+		
+		visitor.visit(compUnit, variableData);
+		
+		verify(variableData, times(2)).setVariableType("int");
+		verify(variableData).setVariableType("String");
+		verify(variableData).setVariableType("VariableIdentificationTestTarget");
+
+	}
+	
+	
 }
