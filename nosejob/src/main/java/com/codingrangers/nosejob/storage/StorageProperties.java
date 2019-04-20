3f289e8cdbe39d1858454f6f44e2b33aae3ff77b
William Akinsanya <akinsanya.will@gmail.com>
2019-04-20 08:45:09 +0100

    moved codestore

--- nosejob/src/main/java/com/codingrangers/nosejob/storage/StorageProperties.java
+++ nosejob/src/main/java/com/codingrangers/nosejob/storage/StorageProperties.java
@@ -3,8 +3,6 @@ package com.codingrangers.nosejob.storage;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
 
 @Configuration
 @ConfigurationProperties("storage")
@@ -13,13 +11,8 @@ public class StorageProperties {
 	private String location;
 
 	public String getLocation() {
-		Path unixPath = Paths.get("nosejob", "src", "codestore");
-		Path windowsPath = Paths.get("src", "codestore");
-		if (Files.exists(unixPath)) {
-			this.location = unixPath.toString();
-		} else if (Files.exists(windowsPath)) {
-			this.location = windowsPath.toString();
-		}
+		Path path = Path.of("classpath:codestore");
+		this.location = path.toString();
 		return location;
 	}
 
