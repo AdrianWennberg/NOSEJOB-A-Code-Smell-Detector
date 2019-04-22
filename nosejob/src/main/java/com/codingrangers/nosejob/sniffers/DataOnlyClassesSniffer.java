eeb52d017d6884f1d061e03f3f448212d7471a74
AleBaccin <baccin.alessandro@gmail.com>
2019-04-22 11:22:23 +0100

    Feature envy modified and tested

--- nosejob/src/main/java/com/codingrangers/nosejob/sniffers/DataOnlyClassesSniffer.java
+++ nosejob/src/main/java/com/codingrangers/nosejob/sniffers/DataOnlyClassesSniffer.java
@@ -18,11 +18,7 @@ public class DataOnlyClassesSniffer extends GeneralSniffer {
             for(String className : currentProjectToAnalyse.getClassNames()){
                 if(!className.equals(currentClassToAnalyse.getName())) {
                     ClassData classToCompareWith = currentProjectToAnalyse.getClassData(className);
-<<<<<<< HEAD
                     calls += currentClassToAnalyse.countMethodCallsTo(classToCompareWith.getFullyQualifiedName());
-=======
-                    calls += currentClassToAnalyse.getMethodCallsTo(classToCompareWith.getFullyQualifiedName()).size();
->>>>>>> Inappropriate Intamcy Sniffer Tested
                 }
             }
 
@@ -30,11 +26,7 @@ public class DataOnlyClassesSniffer extends GeneralSniffer {
         }
 
         private int countTotalMethodsDeclaredAndCalled(){
-<<<<<<< HEAD
             return countHowManyExternalMethodsItsUsing() + currentClassToAnalyse.countInternalMethodCalls() + currentClassToAnalyse.countMethods();
-=======
-            return countHowManyExternalMethodsItsUsing() + currentClassToAnalyse.getInternalMethodCalls().size() + currentClassToAnalyse.getMethodSignatures().size();
->>>>>>> Inappropriate Intamcy Sniffer Tested
         }
 
         @Override
@@ -54,11 +46,7 @@ public class DataOnlyClassesSniffer extends GeneralSniffer {
 
         @Override
         public float getSmellSeverity() {
-<<<<<<< HEAD
             return (countTotalMethodsDeclaredAndCalled() > 4) ? 0f : 0.75f;
-=======
-            return (countTotalMethodsDeclaredAndCalled() > 3) ? 1 : 0;
->>>>>>> Inappropriate Intamcy Sniffer Tested
         }
     }
 
@@ -66,34 +54,22 @@ public class DataOnlyClassesSniffer extends GeneralSniffer {
         if (currentClassToAnalyse.equals(null))
             throw new NullPointerException("Cannot analyse methods of a null.");
 
-<<<<<<< HEAD
-        Smell classDiagnosis = new ClassDiagnosis();
-        classDiagnosis.setCodeData(currentClassToAnalyse);
-        smells.add(classDiagnosis);
-=======
         if (currentClassToAnalyse.getFieldsNames().size() > 0) {
             Smell classDiagnosis = new ClassDiagnosis();
             classDiagnosis.setCodeData(currentClassToAnalyse);
             smells.add(classDiagnosis);
         }
->>>>>>> Inappropriate Intamcy Sniffer Tested
     }
 
     private void retrieveSmellsFromClasses() {
         if (currentProjectToAnalyse.equals(null))
             throw new NullPointerException("Cannot analyse a null project.");
 
-<<<<<<< HEAD
         if(currentProjectToAnalyse.getClassNames().size() > 1) {
             for (String className : currentProjectToAnalyse.getClassNames()) {
                 ClassData currentClassToAnalyse = currentProjectToAnalyse.getClassData(className);
                 retrieveSmellFromSingularClass(currentClassToAnalyse);
             }
-=======
-        for(String className : currentProjectToAnalyse.getClassNames()){
-            ClassData currentClassToAnalyse = currentProjectToAnalyse.getClassData(className);
-            retrieveSmellFromSingularClass(currentClassToAnalyse);
->>>>>>> Inappropriate Intamcy Sniffer Tested
         }
     }
 
