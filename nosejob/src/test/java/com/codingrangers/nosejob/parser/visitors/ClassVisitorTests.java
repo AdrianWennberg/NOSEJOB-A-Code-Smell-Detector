a55e1930b0fb4075aeb9e11087b4065aecc38a53
AleBaccin <baccin.alessandro@gmail.com>
2019-04-22 18:38:41 +0100

    Fixed class visitor test

--- nosejob/src/test/java/com/codingrangers/nosejob/parser/visitors/ClassVisitorTests.java
+++ nosejob/src/test/java/com/codingrangers/nosejob/parser/visitors/ClassVisitorTests.java
@@ -12,10 +12,14 @@ import org.mockito.Mockito;
 =======
 import com.codingrangers.nosejob.parser.data.ParsedClass;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.codingrangers.nosejob.parser.data.ParsedMethod;
 >>>>>>> Solved conflicts
 =======
 >>>>>>> Solved more conflicts
+=======
+import com.codingrangers.nosejob.parser.data.ParsedMethod;
+>>>>>>> Fixed class visitor test
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
@@ -68,6 +72,7 @@ public class ClassVisitorTests {
 		visitor.visit(compUnit[1], ClassData);
 		visitor.visit(compUnit[2], ClassData);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 
@@ -123,14 +128,62 @@ public class ClassVisitorTests {
 <<<<<<< HEAD
 }
 =======
+=======
+	ClassVisitor visitor;
+	ParsedClass ClassData;
+
+	@Before
+	public void before() {
+		visitor = new ClassVisitor(Mockito.mock(MethodVisitor.class),Mockito.mock(VariableVisitor.class));
+		ClassData = Mockito.mock(ParsedClass.class);
+	}
+
+	CompilationUnit getCompUnit(String fileName) {
+		CompilationUnit compUnit = null;
+
+		File file = new File(fileName);
+		try {
+			compUnit = JavaParser.parse(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return compUnit;
+	}
+
+	@Test
+	public void referenceTests() {
+		File referenceTestFile = new File("src/test/ParserTestTargets/ReferenceTestTargets");
+		JavaParserTypeSolver javaParserSolver = new JavaParserTypeSolver(referenceTestFile);
+		ReflectionTypeSolver refelctionSolver = new ReflectionTypeSolver();
+
+		CombinedTypeSolver typeSolver = new CombinedTypeSolver(javaParserSolver,refelctionSolver);
+
+		JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+		JavaParser.getStaticConfiguration().setSymbolResolver(symbolSolver);
+
+
+		CompilationUnit compUnit[] = {getCompUnit("src/test/ParserTestTargets/ReferenceTestTargets/referenceTests/ReferenceTest1.java")
+				,getCompUnit("src/test/ParserTestTargets/ReferenceTestTargets/referenceTests/ReferenceTest2.java")
+				,getCompUnit("src/test/ParserTestTargets/ReferenceTestTargets/referenceTests/ReferenceTest3.java")};
+
+		visitor.visit(compUnit[0], ClassData);
+		visitor.visit(compUnit[1], ClassData);
+		visitor.visit(compUnit[2], ClassData);
+
+>>>>>>> Fixed class visitor test
 		verify(ClassData).addReferenceToMethod("referenceTests.ReferenceTest1", "staticMethodToCall()");
 		verify(ClassData).addReferenceToField("referenceTests.ReferenceTest3", "test");
 		
 		verify(ClassData,never()).addReferenceToMethod("referenceTests.ReferenceTest1", "methodToCall()");
 		verify(ClassData,never()).addReferenceToField("referenceTests.ReferenceTest1", "field");
 	}
+<<<<<<< HEAD
 }
 >>>>>>> Solved conflicts
 =======
 }
 >>>>>>> Solved more conflicts
+=======
+}
+>>>>>>> Fixed class visitor test
